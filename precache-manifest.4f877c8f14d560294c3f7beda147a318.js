504b24971aa5b82718352440ca0442c9fbe532d6
aashis-kishore <aashiskishore@gmail.com>
2020-09-18 18:11:26 +0530

    Update home page

---
+++ precache-manifest.4f877c8f14d560294c3f7beda147a318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb0a10677d3bc7a56b53f84a15c6384",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "8e8f1bcf5a35be3abf2a",
+    "url": "/build/static/js/2.ea45dd73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.ea45dd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8a4ce0dc57e6a3cff8",
+    "url": "/build/static/js/main.e54d54f3.chunk.js"
+  },
+  {
+    "revision": "a7f64e807e31f25021de",
+    "url": "/build/static/js/runtime-main.bfd6a135.js"
+  }
+]);
