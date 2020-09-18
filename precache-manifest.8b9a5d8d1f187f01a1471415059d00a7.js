78c598d60eb3348e5582b6dd517964bc8ecd5311
aashis-kishore <aashiskishore@gmail.com>
2020-09-18 18:13:41 +0530

    Update production build

---
+++ precache-manifest.8b9a5d8d1f187f01a1471415059d00a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7b13849d484a396f59516d9ea8013e",
+    "url": "/notedown/index.html"
+  },
+  {
+    "revision": "ae3d4c786a03f92e9d28",
+    "url": "/notedown/static/js/2.3e31d55a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notedown/static/js/2.3e31d55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8a4ce0dc57e6a3cff8",
+    "url": "/notedown/static/js/main.e54d54f3.chunk.js"
+  },
+  {
+    "revision": "558da8c2bfad12d61e1e",
+    "url": "/notedown/static/js/runtime-main.80f8cffd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.4f877c8f14d560294c3f7beda147a318.js"
+  "/notedown/precache-manifest.8b9a5d8d1f187f01a1471415059d00a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notedown/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
