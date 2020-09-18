6a146de3dfaf3ab8efdaa4d00c8fc6f36a3cce11
aashis-kishore <aashiskishore@gmail.com>
2020-09-18 17:54:46 +0530

    Add build folder for github pages

---
+++ precache-manifest.1184401756cb98fd044e66881992c6a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6dc952e838d64f067a8545c84ac5a0",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "f99cafd1948ade196f6b",
+    "url": "/build/static/js/2.90f35217.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.90f35217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985c74f790e433405c7d",
+    "url": "/build/static/js/main.c70a5f36.chunk.js"
+  },
+  {
+    "revision": "a7f64e807e31f25021de",
+    "url": "/build/static/js/runtime-main.bfd6a135.js"
+  }
+]);
