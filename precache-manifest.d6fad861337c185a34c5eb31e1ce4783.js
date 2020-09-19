6390a79fa88f1d837ceb08f1595365e1678d223f
aashis-kishore <aashiskishore@gmail.com>
2020-09-19 13:08:13 +0530

    New production build

---
+++ precache-manifest.d6fad861337c185a34c5eb31e1ce4783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c51d01c1e7171ec585e251d52023c0",
+    "url": "/notedown/index.html"
+  },
+  {
+    "revision": "4e254bdfb86df40904d0",
+    "url": "/notedown/static/js/2.fe94579f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notedown/static/js/2.fe94579f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e56a0603a04394f19d",
+    "url": "/notedown/static/js/main.b846681e.chunk.js"
+  },
+  {
+    "revision": "558da8c2bfad12d61e1e",
+    "url": "/notedown/static/js/runtime-main.80f8cffd.js"
+  }
+]);
